e343f02cf469634d6df55bc404b7e8ab13dbc4f9
Thien Nguyen <65026941+Thien-Nguyen-Git@users.noreply.github.com>
2023-04-27 01:24:55 -0400

    Implementing Alert on Registration
    On the registration page, I am setting an alert, so that users can get a notification, if no values are being input.

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,6 @@
 import React, { useReducer, useContext } from "react";
 import reducer from "./reducer";
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const initialState = {
   isLoading: false,
@@ -13,8 +14,22 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    clearAlert();
+  };
+
+  const clearAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT });
+    }, 3000);
+  };
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,5 +1,25 @@
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
+
 const reducer = (state, action) => {
-  throw new Error(`no sch action: ${action.type}`);
+  if (action.type === DISPLAY_ALERT) {
+    return {
+      ...state,
+      showAlert: true,
+      alertType: "danger",
+      alertText: "Please provide all values!",
+    };
+  }
+
+  if (action.type === CLEAR_ALERT) {
+    return {
+      ...state,
+      showAlert: false,
+      alertType: "",
+      alertText: "!",
+    };
+  }
+
+  throw new Error(`no such action : ${action.type}`);
 };
 
 export default reducer;
